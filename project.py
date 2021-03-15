b3bc0e05c40ea01c0fbcc4dc831e108a2be03947
ZINGIEV001 <77277556+ZINGIEV001@users.noreply.github.com>
2021-03-15 20:53:45 +0300

    3

--- project.py
+++ project.py
@@ -15,27 +15,25 @@ browser.find_element_by_name('password').send_keys(password)
 browser.find_element_by_name('password').send_keys(Keys.ENTER)
 time.sleep(3)
 
-one_link = ['']
-
-links_posts = browser.find_elements_by_tag_name('a')
-posts_urls = [item.get_attribute('href') for item in links_posts if '/p/' in item.get_attribute('href')]
+one_link = [' ']
 
 def sendComment():
-    if one_link[-1] != posts_urls[0]:
-        one_link.append(posts_urls[0])
-        send_comment = browser.find_element_by_class_name('Ypffh')
-        send_comment.click()
-        send_comment = browser.find_element_by_class_name('Ypffh')
-        send_comment.send_keys('Тестовый комментарии')
-        send_comment.send_keys(Keys.ENTER)
-
-while True:
-    browser.get('https://instagram.com/yasinzingiev')
-    time.sleep(3)
-
     links_posts = browser.find_elements_by_tag_name('a')
     posts_urls = [item.get_attribute('href') for item in links_posts if '/p/' in item.get_attribute('href')]
+    time.sleep(2)
+    browser.get('https://instagram.com/yasinzingiev')
+    time.sleep(3)
+    if posts_urls[0] != one_link[-1]:
+        one_link.append(posts_urls[0])
+        while range(3):
+            browser.get(posts_urls[0])
+            send_comment = browser.find_element_by_class_name('Ypffh')
+            send_comment.click()
+            send_comment = browser.find_element_by_class_name('Ypffh')
+            send_comment.send_keys('Тестовый комментарии')
+            send_comment.send_keys(Keys.ENTER)
+    else:
+        print('continue')
 
-
-
+while True:
     sendComment()
