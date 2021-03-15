f069e1899b634eac40b1e683400f54e4c4445c88
ZINGIEV001 <77277556+ZINGIEV001@users.noreply.github.com>
2021-03-15 20:35:55 +0300

    2

--- project.py
+++ project.py
@@ -15,3 +15,27 @@ browser.find_element_by_name('password').send_keys(password)
 browser.find_element_by_name('password').send_keys(Keys.ENTER)
 time.sleep(3)
 
+one_link = ['']
+
+links_posts = browser.find_elements_by_tag_name('a')
+posts_urls = [item.get_attribute('href') for item in links_posts if '/p/' in item.get_attribute('href')]
+
+def sendComment():
+    if one_link[-1] != posts_urls[0]:
+        one_link.append(posts_urls[0])
+        send_comment = browser.find_element_by_class_name('Ypffh')
+        send_comment.click()
+        send_comment = browser.find_element_by_class_name('Ypffh')
+        send_comment.send_keys('Тестовый комментарии')
+        send_comment.send_keys(Keys.ENTER)
+
+while True:
+    browser.get('https://instagram.com/yasinzingiev')
+    time.sleep(3)
+
+    links_posts = browser.find_elements_by_tag_name('a')
+    posts_urls = [item.get_attribute('href') for item in links_posts if '/p/' in item.get_attribute('href')]
+
+
+
+    sendComment()
