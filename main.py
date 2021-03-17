ca82f77ca8d07ec60477dcd0de13204546974380
ZINGIEV001 <77277556+ZINGIEV001@users.noreply.github.com>
2021-03-17 15:15:48 +0300

    save

--- main.py
+++ main.py
@@ -7,69 +7,61 @@ import time
 from comments import commentaries
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-bot = telebot.TeleBot('1696728315:AAG7PwNqsyj6d2r9yZ59YceuFOzqUUhdavs')
 
+ua = dict(DesiredCapabilities.CHROME)
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x935')
+browser = webdriver.Chrome(chrome_options=options)
+browser.get('https://instagram.com')
 
-@bot.message_handler(commands=['start'])
-def starts(message):
-    bot.send_message(message.chat_id, 'Программа запущена')
-    ua = dict(DesiredCapabilities.CHROME)
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('window-size=1920x935')
-    browser = webdriver.Chrome(chrome_options=options)
-    browser.get('https://instagram.com')
+time.sleep(3.5)
 
-    time.sleep(3.5)
+browser.find_element_by_name('username').send_keys(username)
+browser.find_element_by_name('password').send_keys(password)
+browser.find_element_by_name('password').send_keys(Keys.ENTER)
+time.sleep(3)
 
-    browser.find_element_by_name('username').send_keys(username)
-    browser.find_element_by_name('password').send_keys(password)
-    browser.find_element_by_name('password').send_keys(Keys.ENTER)
-    time.sleep(3)
+one_link = []
 
-    one_link = []
+try:
+    def sendComment():
+        if len(one_link) == 33:
+            one_link = []
+        browser.get('https://instagram.com/app.python')
+        time.sleep(3)
 
-    try:
-        def sendComment():
-            if len(one_link) == 33:
-                one_link = []
-            browser.get('https://instagram.com/app.python')
-            time.sleep(3)
+        links_posts = browser.find_elements_by_tag_name('a')
+        posts_urls = [item.get_attribute(
+            'href') for item in links_posts if '/p/' in item.get_attribute('href')]
+        time.sleep(2)
 
-            links_posts = browser.find_elements_by_tag_name('a')
-            posts_urls = [item.get_attribute(
-                'href') for item in links_posts if '/p/' in item.get_attribute('href')]
+        if posts_urls[0] not in one_link:
+            one_link.append(posts_urls[0])
+
+            browser.get(posts_urls[0])
             time.sleep(2)
 
-            if posts_urls[0] not in one_link:
-                one_link.append(posts_urls[0])
+            btn_like = browser.find_element_by_class_name('fr66n')
+            btn_like.click()
+            time.sleep(0.5)
 
+            for i in range(3):
+                global commentaries
                 browser.get(posts_urls[0])
                 time.sleep(2)
+                send_comment = browser.find_element_by_class_name('Ypffh')
+                send_comment.click()
 
-                btn_like = browser.find_element_by_class_name('fr66n')
-                btn_like.click()
-                time.sleep(0.5)
+                send_comment = browser.find_element_by_class_name('Ypffh')
+                send_comment.send_keys(commentaries())
+                time.sleep(2)
 
-                for i in range(3):
-                    global commentaries
-                    browser.get(posts_urls[0])
-                    time.sleep(2)
-                    send_comment = browser.find_element_by_class_name('Ypffh')
-                    send_comment.click()
+                send_comment.send_keys(Keys.ENTER)
+                time.sleep(2)
 
-                    send_comment = browser.find_element_by_class_name('Ypffh')
-                    send_comment.send_keys(commentaries())
-                    time.sleep(2)
+except Exception as ex:
+    print(ex)
 
-                    send_comment.send_keys(Keys.ENTER)
-                    time.sleep(2)
-
-    except Exception:
-        bot.send_message(message.chat.id, 'Ошибка. Программа приостоновлена')
-
-    while True:
-        sendComment()
-
-
-bot.polling()
+while True:
+    sendComment()
